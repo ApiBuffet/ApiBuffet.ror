0fe6f050ac22173ba1347465062311cc9206ee05
David Wayne Baxter <david.w.baxter@gmail.com>
2014-01-28 23:31:21 -0800

    Initial commit of template files. No customization at this point.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ApiBuffetRor::Application.config.secret_key_base = 'da3b1d91037d4c481e3186fff40acc6adafc72ce60a8a5dee038d191065689025f77f50aed730a48505893c43e2ce1406467c4c5aca6ca718cc218978183eb9e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ApiBuffetRor::Application.config.session_store :cookie_store, key: '_ApiBuffet_ror_session'
